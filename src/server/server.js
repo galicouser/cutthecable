5c52795de0d582d08a58b83817c5870521644212
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2024-03-20 15:47:50 -0700

    party repo initalize

---
+++ src/server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../../public')));
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+module.exports = () => {
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: './src/index.jsx',
+    mode: "development",
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve('./public'),
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: '/node_modules/',
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", 'css-loader'],
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys)
+    ],
+    devServer: { historyApiFallback: true }
+  }
+}
