3bbad2c1f113b8c76ff0cb982844ef0cc01e888e
amberjpdev <amber@galico.io>
2024-06-11 14:14:19 -0400

    create users state

---
+++ src/components/actions/userActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+import { FETCH_ALL_USERS, SELECT_SUBSCRIPTION_PLAN } from './types';
+
+export const fetchAllUsers = () => async (dispatch) => {
+  try {
+    const response = await axios.get('/auth/get-users');
+
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch users');
+    }
+
+    const { data } = response.data;
+    console.
+    dispatch({ type: 'FETCH_ALL_USERS', payload: data });
+  } catch (error) {
+    dispatch({ type: 'FETCH_SUBSCRIPTION_PLANS_FAILURE', payload: error.message })
+  }
+};
+
+
+
--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import subscriptionReducer from './subscriptionReducer'; // Import your subscription reducer
+import userReducer from './userReducer'; // Import your subscription reducer
 
 // Combine all reducers into a root reducer
 const rootReducer = combineReducers({
   subscription: subscriptionReducer,
+  user: userReducer
   // Add more reducers as needed
 });
 
